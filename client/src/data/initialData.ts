fe314b4874c5374fbbeae3627d95ba0854ee97c4
almat-vitalogy <almat@vitalogy.ai>
2025-01-22 18:07:05 +0800

    using axios everywhere

--- client/src/data/initialData.ts
+++ client/src/data/initialData.ts
@@ -1,32 +1,20 @@
-// need customerlist, customer_label, daily_message tables
-
+import axios from "axios";
 import { Chat, Label } from "../types";
 
 export const fetchChats = async (): Promise<Chat[]> => {
   try {
-    // @ts-ignore
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/customers/chats`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    // console.log("checking fetch")
-    return data;
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/customers/chats`);
+    return response.data;
   } catch (error) {
-    console.error("Error fetching chats: 123", error);
+    console.error("Error fetching chats:", error);
     return [];
   }
 };
 
 export const getLabels = async (): Promise<Label[]> => {
   try {
-    // @ts-ignore
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/customers/getTotalLabels`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    return data;
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/customers/getTotalLabels`);
+    return response.data;
   } catch (error) {
     console.error("Error fetching labels:", error);
     return [];
